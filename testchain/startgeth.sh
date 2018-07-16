c23cdc315db14ebcb22ceaec529f6ed36c6271a4
yehorhromadskyi <egromad27@gmail.com>
2018-07-16 17:21:16 +0000

    commented for test purposes

--- testchain/startgeth.sh
+++ testchain/startgeth.sh
@@ -3,8 +3,8 @@ rm -rf devChain/dapp
 rm -rf devChain/nodes
 rm -rf devchain/nodekey
 
-$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+#$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 #& $scriptDir\IndexSymbols.ps1
 
-$scriptDir\geth.exe --datadir=devChain init genesis_clique.json
-$scriptDir\geth.exe --nodiscover --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --unlock 0x12890d2cce102216644c59dae5baed380d84830c --password "pass.txt" --verbosity 0 console  
+$PSScriptRoot\geth.exe --datadir=devChain init genesis_clique.json
+$PSScriptRoot\geth.exe --nodiscover --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --unlock 0x12890d2cce102216644c59dae5baed380d84830c --password "pass.txt" --verbosity 0 console  
